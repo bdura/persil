4a13a12094463e6c4d478747412f9976362194b2
Basile Dura <basile@bdura.me>
2026-04-11 22:13:22 +0200

    test: line_info parser

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,14 @@
 from persil import index, string
+from persil.parsers.index import line_info
+from persil.utils import RowCol
 
 
 def test_index():
     parser = string("test") >> index()
     assert parser.parse("test") == 4
+
+
+def test_line_info_parser():
+    parser = string("hello\n") >> line_info
+    result = parser.parse("hello\n")
+    assert result == RowCol(index=6, row=1, col=0)
